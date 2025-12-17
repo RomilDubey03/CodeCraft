6d5b506e7bd276db02ac01af00d6ced8398eab2f
Romil Dubey <romildubey03@gmail.com>
2025-12-17 12:39:55 +0530

    deploying attempt

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,29 +2,28 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = new express();
+const app = express();
 
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
-        credentials: true,
+        credentials: true
     })
 );
 
-app.use(express.json({limti : '50mb'}));
+app.use(express.json({ limti: "50mb" }));
 app.use(cookieParser());
-app.use(express.urlencoded({extended: true, limit: '50mb'}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
 
 //routes import
-import userRouter from './routes/user.routes.js';
-import problemRouter from './routes/problem.routes.js';
-import submissionRouter from './routes/submission.routes.js';
+import userRouter from "./routes/user.routes.js";
+import problemRouter from "./routes/problem.routes.js";
+import submissionRouter from "./routes/submission.routes.js";
 // http://localhost:3000
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/problems", problemRouter);
 app.use("/api/v1/submissions", submissionRouter);
-
 
 export default app;
 // CORS configuration updated
